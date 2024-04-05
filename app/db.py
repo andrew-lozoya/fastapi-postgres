60dec0963126c18385b6c7732b6bc01bbdd01310
Andrew Lozoya <lozoyaatl@att.net>
2024-04-05 01:26:49 -0500

    initial push

---
+++ app/db.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+import os
+
+#engine object to connect to db
+engine = create_async_engine(
+    url= os.getenv('DATABASE_URL'),
+    echo = True
+)
+#base class for creating database models
+class Base(DeclarativeBase):
+    pass
